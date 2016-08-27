75645fff1fff68aefcc07e0201bf3b7a67b93595
Yosia <yosia89@yahoo.com>
2016-08-27 09:03:13 +0700

    update vagrant file

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,3 +1,8 @@
+#!/usr/bin/env bash
+
 apt-get update
 apt-get upgrade
-apt-get install mysql-server-5.6
+debconf-set-selections <<< 'mysql-server-5.6 mysql-server/root_password password exodia89'
+debconf-set-selections <<< 'mysql-server-5.6 mysql-server/root_password_again password exodia89'
+apt-get -y install mysql-server-core-5.6
+apt-get -y install mysql-server-5.6
